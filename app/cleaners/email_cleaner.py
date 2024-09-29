b8bf6a061f6b7ab00a5f344aba846034a4c3145f
Maxim Kirilyuk <werserk@inbox.ru>
2024-09-29 08:47:05 +0300

    email cleaning update

--- app/cleaners/email_cleaner.py
+++ app/cleaners/email_cleaner.py
@@ -7,8 +7,8 @@ class EmailCleaner(BaseCleaner):
     def clean(self, email: str) -> str:
         # remove all non-alphanumeric characters but not the @ and _ symbol
         email = str(email)
-        email = re.sub(r"[^a-zA-Z0-9@_]", "", email)
+        email = email.lower()
+        email = re.sub(r"[^a-z0-9@_]", "", email)
         for i in range(10):
             email = email.split(f"{i}")[0]
-        email = email.lower()
         return email
